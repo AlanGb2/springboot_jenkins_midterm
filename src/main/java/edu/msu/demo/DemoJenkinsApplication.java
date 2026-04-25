f35abd27229a7ce905dd0c57d61e53f61859c5d8
AlanGb2 <gonzalezbraa2@montclair.edu>
2026-04-25 19:20:11 -0400

    Update DemoJenkinsApplication.java

--- src/main/java/edu/msu/demo/DemoJenkinsApplication.java
+++ src/main/java/edu/msu/demo/DemoJenkinsApplication.java
@@ -2,12 +2,19 @@ package edu.msu.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoJenkinsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoJenkinsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJenkinsApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot running on Kubernetes!";
+    }
 }
